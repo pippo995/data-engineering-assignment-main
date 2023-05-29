fda5d4954c181921a3222c69c88562735c5407ca
ScottiFi <pippo995@gmail.com>
2023-05-29 11:35:11 +0200

    fixec test

--- app/test.py
+++ app/test.py
@@ -11,20 +11,19 @@ class TestCode(unittest.TestCase):
 
     # Create a test csv file with some employee data
     with open('test.csv', 'w') as file:
-      file.write('FirstName,LastName,BirthDate,Salary\n')
-      file.write('     Alice,Smith,1990-01-01,45000\n')
-      file.write('Bob    , Jones ,1985-05-15,75000\n')
-      file.write('Charlie,Brown,1975-12-25,120000\n')
+      file.write('FirstName,LastName,BirthDate,Department,Salary\n')
+      file.write('     Alice,Smith,1990-01-01,Marketing,45000\n')
+      file.write('Bob    , Jones ,1985-05-15,Finance  ,A75000\n')
+      file.write('Charlie,Brown,1975-12-25, Sales,120000\n')
 
     # Create a test dataframe with the expected transformed data
     self.expected_df = pd.DataFrame({
+      'Department': ['Marketing', 'Finance', 'Sales'],
       'Salary': [45000, 75000, 120000],
       'FullName': ['Alice Smith', 'Bob Jones', 'Charlie Brown'],
       'Age': [33, 37, 47],
       'SalaryBucket': ['A', 'B', 'C']
     })
-
-    print(self.expected_df)
 
   
   def tearDown(self):
@@ -36,8 +35,8 @@ class TestCode(unittest.TestCase):
     df = read_csv('test.csv')
     
     self.assertIsNotNone(df)
-    self.assertEqual(df.shape, (3, 4))
-    self.assertListEqual(list(df.columns), ['FirstName', 'LastName', 'BirthDate', 'Salary'])
+    self.assertEqual(df.shape, (3, 5))
+    self.assertListEqual(list(df.columns), ['FirstName', 'LastName', 'BirthDate', 'Department', 'Salary'])
 
   
   def test_transform_data(self):
@@ -46,8 +45,8 @@ class TestCode(unittest.TestCase):
     df = transform_data(df)
     
     self.assertIsNotNone(df)
-    self.assertEqual(df.shape, (3, 4))
-    self.assertListEqual(list(df.columns), ['Salary', 'FullName', 'Age', 'SalaryBucket'])
+    self.assertEqual(df.shape, (3, 5))
+    self.assertListEqual(list(df.columns), ['Department', 'Salary', 'FullName', 'Age', 'SalaryBucket'])
     
     pd.testing.assert_frame_equal(df, self.expected_df)
 
